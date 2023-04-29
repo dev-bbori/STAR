eec29e279103ee11b48d0e5faad3225f3d90371b
bbori.Kim <dev.bbori@gmail.com>
2023-04-30 01:37:08 +0900

    20230430 김보령

--- star/src/com/star/main/MainFrontController.java
+++ star/src/com/star/main/MainFrontController.java
@@ -22,8 +22,6 @@ public class MainFrontController extends HttpServlet {
 		if(target.equals("main")) {
 			result = new SelectCurrentSitterController().execute(req, resp);
 			System.out.println("호잇");
-		}else if() {
-			
 		}
 		    
 	
